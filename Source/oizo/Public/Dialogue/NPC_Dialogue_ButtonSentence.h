cc64be751a242a6a4678d4a3919255c62c230a6d
Louis Celeyron <louis.celeyron@gmail.com>
2021-09-05 17:54:32 +0200

    La c'est stable

--- Source/oizo/Public/Dialogue/NPC_Dialogue_ButtonSentence.h
+++ Source/oizo/Public/Dialogue/NPC_Dialogue_ButtonSentence.h
@@ -1,18 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "Dialogue/DialogueNPC.h"
 #include "CoreMinimal.h"
-
-#include <activation.h>
-
 #include "Blueprint/UserWidget.h"
 #include "NPC_Dialogue_ButtonSentence.generated.h"
 
 /**
- * 
- */
+* 
+*/
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FIntDelegate,int,Index);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTransitionNPCDDelegate,FTransition,Transitoion);
@@ -21,7 +16,7 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FV2DintDelegate,FVector2D,Drag,int,
 UCLASS(Blueprintable)
 class OIZO_API UNPC_Dialogue_ButtonSentence : public UUserWidget
 {
-private:
+	public:
 	GENERATED_BODY()
 
 	public:
@@ -37,8 +32,8 @@ private:
 	FTransitionNPCDDelegate OnTransitionClick;
 	UPROPERTY(BlueprintCallable,BlueprintAssignable)
 	FIntDelegate OnSentenceClick;
-	
+    
 	public:
 	UFUNCTION(BlueprintCallable,BlueprintPure)
-	FSentence GetSentenceContent() const;	
+    FSentence GetSentenceContent() const;    
 };
